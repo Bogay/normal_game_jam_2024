002c4499c5edbd7e2298561a63e293d4a68d3b82
bogay <pojay11523@gmail.com>
2024-05-18 20:53:22 +0800

    refactor: extract battle::lv0

--- src/battle/mod.rs
+++ src/battle/mod.rs
@@ -1,8 +1,8 @@
+mod lv0;
+pub use lv0::EnemyLevel0;
+
 use crate::app::{AppResult, Bullet, Player};
-use ratatui::{
-    prelude::*,
-    widgets::canvas::{Circle, Shape},
-};
+use ratatui::widgets::canvas::Shape;
 use std::{fmt::Debug, time::Duration};
 
 #[derive(Debug, Clone, Copy)]
@@ -25,84 +25,5 @@ pub struct DrawEnemy<'a>(pub &'a Box<dyn Enemy>);
 impl Shape for DrawEnemy<'_> {
     fn draw(&self, painter: &mut ratatui::widgets::canvas::Painter) {
         self.0.draw(painter);
-    }
-}
-
-#[derive(Debug)]
-pub struct EnemyLevel0 {
-    pos_x: f64,
-    pos_y: f64,
-    hp: isize,
-    timer: Duration,
-    bullets: Vec<Bullet>,
-}
-
-impl EnemyLevel0 {
-    pub fn new() -> Self {
-        Self {
-            pos_x: 30.,
-            pos_y: 30.,
-            hp: 10,
-            timer: Duration::ZERO,
-            bullets: vec![],
-        }
-    }
-
-    fn new_bullet(&self, player: &Player) -> Bullet {
-        let delta_x = player.pos_x - self.pos_x;
-        let delta_y = player.pos_y - self.pos_y;
-        let (delta_x, delta_y) = crate::norm(delta_x, delta_y);
-
-        const VELOCITY: f64 = 6.;
-        const OFFSET: f64 = 2.5;
-        Bullet {
-            pos_x: self.pos_x + OFFSET * delta_x,
-            pos_y: self.pos_y + OFFSET * delta_y,
-            velocity_x: delta_x * VELOCITY,
-            velocity_y: delta_y * VELOCITY,
-            is_player: false,
-            ..Default::default()
-        }
-    }
-}
-
-impl Shape for EnemyLevel0 {
-    fn draw(&self, painter: &mut ratatui::widgets::canvas::Painter) {
-        let circle = Circle {
-            x: self.pos_x,
-            y: self.pos_y,
-            radius: 3.,
-            color: Color::Red,
-        };
-        circle.draw(painter);
-    }
-}
-
-impl Enemy for EnemyLevel0 {
-    fn tick(&mut self, delta: Duration, player: &mut Player) -> AppResult<EnemyAction> {
-        if self.hp <= 0 {
-            return Ok(EnemyAction::Die);
-        }
-
-        self.timer += delta;
-        if self.timer > Duration::from_secs_f32(2.) {
-            self.timer = Duration::ZERO;
-            self.bullets.push(self.new_bullet(player));
-        }
-
-        Ok(EnemyAction::Idle)
-    }
-
-    fn bullets(&mut self) -> Vec<Bullet> {
-        self.bullets.drain(..).collect()
-    }
-
-    fn hurt(&mut self, bullets: &mut Vec<Bullet>) {
-        for b in bullets {
-            if b.is_player && crate::dis(b.pos_x, b.pos_y, self.pos_x, self.pos_y) <= 3. {
-                b.will_remove = true;
-                self.hp -= 1;
-            }
-        }
     }
 }
